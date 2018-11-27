cf7b40d6120d0983057a83e8516674e7a20d91e8
Rusklim24 <rusklim22@gmail.com>
2018-11-27 22:53:00 +0300

    'Artur'

---
+++ srednee_arifm.py
@@ -0,0 +1,9 @@
+try:
+    a = int(input('Vvedite chislo:'))
+    b = int(input('Vvedite vtoroe chislo:'))
+    c = (a + b) / 2
+    print('Srednee arifmiticheskoe = ', c)
+except:
+    print('Vvedite chisla, ne bukvy')
+
+
